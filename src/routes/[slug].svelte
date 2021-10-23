e3b78e6affd4462c8fbf3fae20175614da83eb53
JUan Andrade <jfandradea@gmail.com>
2021-10-22 23:34:13 -0500

    render slices in html

--- src/routes/[slug].svelte
+++ src/routes/[slug].svelte
@@ -1,5 +1,6 @@
 <script context="module">
 	import Client from '$lib/prismic';
+
 	export async function load({ page }) {
 		const response = await Client.getByUID('recipe', page.params.slug);
 		const recipe = await response;
@@ -14,9 +15,33 @@
 
 <script>
 	import PrismicDom from 'prismic-dom';
+	const Elements = PrismicDom.RichText.Elements;
 	export let recipe;
 	const { data } = recipe;
 	console.log(data);
+	function htmlSerializer(type, element, content, children) {
+		switch (type) {
+			// Add a class to paragraph elements
+			case Elements.paragraph:
+				return '<p class="paragraph-class">' + children.join('') + '</p>';
+
+			// Don't wrap images in a <p> tag
+			case Elements.image:
+				return '<img src="' + element.url + '" alt="' + element.alt + '">';
+
+			// Add a class to hyperlinks
+			case Elements.hyperlink:
+				var target = element.data.target
+					? 'target="' + element.data.target + '" rel="noopener"'
+					: '';
+				var linkUrl = PrismicDOM.Link.url(element.data, linkResolver);
+				return '<a class="some-link"' + target + ' href="' + linkUrl + '">' + content + '</a>';
+
+			// Return null to stick with the default behavior for all other elements
+			default:
+				return null;
+		}
+	}
 </script>
 
 {#if data}
@@ -29,13 +54,66 @@
 				Cook time: <span class="mr-2">{PrismicDom.RichText.asText(data.cook_time)} 😋</span>
 				Portions: <span>{data.yield}</span>
 			</div>
-			<img
-				class="my-4 shadow-2xl "
-				src={data.recipe_image.url}
-				alt={data.recipe_image.alt}
-				width={data.recipe_image.dimensions.width}
-				height={data.recipe_image.dimensions.height}
-			/>
+			<div class="flex ">
+				<img
+					class="mt-4 mb-8 shadow-2xl "
+					src={data.recipe_image.url}
+					alt={data.recipe_image.alt}
+					width={data.recipe_image.dimensions.width}
+					height={data.recipe_image.dimensions.height}
+				/>
+			</div>
+			<div class="grid">
+				{#each data.body as slice}
+					{#if slice.slice_type === 'ingredient_group'}
+						<div>
+							<h1 class="my-8 font-bold text-2xl">
+								{PrismicDom.RichText.asText(slice.primary.ingredients_title)}
+							</h1>
+							<ul class="list-disc">
+								{#each slice.items as ingredient}
+									<li class="ml-6">
+										{@html PrismicDom.RichText.asHtml(
+											ingredient.ingredient_item,
+											null,
+											htmlSerializer
+										)}
+									</li>
+								{/each}
+							</ul>
+						</div>
+					{/if}
+				{/each}
+			</div>
+			<div class="grid">
+				{#each data.body as slice}
+					{#if slice.slice_type === 'instructions'}
+						<div>
+							<h1 class="my-8 font-bold text-2xl">Instructions</h1>
+							<ul class="list-disc">
+								{#each slice.items as instruction}
+									<li class="ml-6">
+										{@html PrismicDom.RichText.asHtml(
+											instruction.instruction_item,
+											null,
+											htmlSerializer
+										)}
+									</li>
+								{/each}
+							</ul>
+						</div>
+					{/if}
+				{/each}
+			</div>
 		</div>
 	</article>
 {/if}
+
+<style>
+	.grid {
+		display: grid;
+		grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+		gap: 20px;
+		@apply my-10;
+	}
+</style>
